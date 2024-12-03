1c852112ad0b11590aba63c00acfc3a620e414e4
ChickenPracticeCoding <trungkienhaui03@gmail.com>
2024-12-03 21:10:17 +0700

    up up

---
+++ XayDungfan/XayDungfan/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Sockets;
+
+namespace XayDungfan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            fan fan1 = new fan(fan.MEDIUM, true, 5, "Blue" );
+            fan fan2 = new fan(fan.HIGH, false, 10, "White");
+            Console.WriteLine(fan1.ToString());
+            Console.WriteLine(fan2.ToString());
+        }
+    }
+}
---
+++ XayDungfan/XayDungfan/fan.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XayDungfan
+{
+    internal class fan
+    {
+        public const int SLOW = 1;
+        public const int MEDIUM = 2;
+        public const int HIGH = 3;
+
+        private int speed = SLOW;
+        private bool on = false;
+        private double radius = 5;
+        private string color = "Blue";
+
+
+
+
+        public fan()
+        {
+        }
+
+        public fan(int _speed, bool _on, double _radius, string _color)
+        {
+           speed = _speed;
+            on = _on;
+            radius = _radius;
+            color = _color;
+        }
+
+        public int Speed
+        {
+            get { return speed; }
+            set { speed = value; }
+        }
+
+        public double Radius
+        {
+            get { return radius; }
+            set { radius = value; }
+        }
+
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+
+        public bool On
+        {
+            get { return on; } 
+            set { on = value; }
+        }
+
+        public override string ToString() 
+        {
+            if(on)
+            {
+               return $"Fan is on: Speed = {speed}, Color = {color}, Radius = {radius}";
+            }
+            else
+            {
+                return $"Fan is off: Color = {color}, Radius = {radius}";
+            }
+        }
+
+    }
+}
